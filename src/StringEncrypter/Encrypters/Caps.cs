51606e7a4391232c5888702cfd29f75b4cc61717
Qrz <nzroops@outlook.com>
2021-11-25 13:19:32 +0800

    增加大小写转换

---
+++ src/StringEncrypter/Encrypters/Caps.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEncrypter.Encrypters
+{
+    public class Caps : Encrypter
+    {
+        #region Constructors
+
+        public Caps()
+        {
+            CanDecrypt = true;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public override string Decrypt(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return str;
+
+            return str.ToLower();
+        }
+
+        public override string Encrypt(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return str;
+
+            return str.ToUpper();
+        }
+
+        #endregion Methods
+    }
+}
